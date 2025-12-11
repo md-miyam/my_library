55e92122872abeea2147a73487ae2b5a88e5d38b
md-miyam <mdmiyam20@gmail.com>
2025-12-11 21:07:27 +0600

    my local changes

--- lib/app/data/theme.dart
+++ lib/app/data/theme.dart
@@ -5,7 +5,7 @@ import 'app_text_styles.dart';
 
 ThemeData themeData() {
   return ThemeData(
-    fontFamily: 'Nunito',
+    // fontFamily: 'Nunito',
     scaffoldBackgroundColor: AppColors.whiteColor,
     appBarTheme: AppBarTheme(
       backgroundColor: AppColors.whiteColor,
